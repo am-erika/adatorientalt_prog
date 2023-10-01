58c1b9b372cde010830e9f715179ff53651489d6
erika <erika.murancsik@gmail.com>
2023-10-01 14:19:15 +0200

    fix

--- week4/rejtelyesuzenet.py
+++ week4/rejtelyesuzenet.py
@@ -41,53 +41,8 @@ def decode_char(char):
 
 def main():
     print(decode_text(TEXT), sep = "", end = "")
-    #kisteszt = "yzYZ abc!?  b:k"
-    #betu = ":"
-    #print(decode_char(betu))
-"""
-def decode_text(TEXT):
-    for char in TEXT:
-        decode_char(char)
-        print(char, sep="", end="")
 
 
-def decode_char(char):
-    if ord(char) == 32 or 9 or 10 or 33 or 58 or 46 or 63:   
-        return char
-    
-    elif ord(char) == 121:
-        return char.replace("y", "a")
-    
-    elif ord(char) == 122:
-        return char.replace("z", "b")
-
-    elif ord(char) == 89:
-        return char.replace("Y", "A")
-
-    elif ord(char) == 90:
-        return char.replace("Z", "B")
-    
-    else:
-        new_char = char
-        new_ord = ord(char) + 2
-        new_char = chr(new_ord)
-        return new_char
-
-    # if char == "y":
-    #     return "a"
-    # if char == "z":
-    #     return "b"
-    # if char == "Y":
-    #     return "A"
-    # if char == "Z":
-    #     return "B"
-
-
-
-def main():
-    decode_text(TEXT)
-    
-"""
 ##############################################################################
 
 if __name__ == "__main__":
